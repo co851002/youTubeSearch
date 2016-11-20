4e5ca791dcca8dcd9cfcb61480d25db20804d456
System Administrator <root@Christos-MacBook-Pro.local>
2016-11-20 19:15:46 +0000

    Youtube API and file structure added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyD5oMB3I18VYgmap1IsGFRgPWJoLrE_log';
+
 const App = () => {
-  return <div>Blah Blah Blah</div>
-}
+  return {
+    <div>
+      <SearchBar/>
+    </div>
+   }
+  }
 
 ReactCom.render(<App/>, document.querySelector('.container'))
