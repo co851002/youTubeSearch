27e6bd48eaa3bddd8f972b55c3f5318be9be1ef6
Christo Oosthuizen <COosthuizen@vibrant.corp>
2016-11-21 18:30:02 +0000

    'SearchbarState'

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
-const SearchBar = () => {
-  return <input/>
-};
+class SearchBar extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state= {term: 'Strating Value' };
+  }
+
+  render(){
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={e => this.setState({ term: e.target.value })}
+        />
+
+          Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
+}
 
 export default SearchBar;
